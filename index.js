05fc1b6521fba1b6633b813a4b5f95f1c6b24103
Zhen Wei Wu <zhenwu93@gmail.com>
2018-05-12 02:15:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,10 @@ function deepestChild() {
   }
   return lis;
 }
+
+function increaseRankBy(n) {
+  var lis=document.getElementById("app").querySelectorAll("ul.ranked-list li");
+  for(let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+}
