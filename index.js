db037d63e1a6b63f4f57ca48f4cce8b17ca7d2e3
Zhen Wei Wu <zhenwu93@gmail.com>
2018-05-12 02:12:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector('#nested .target');
 }
+
+function deepestChild() {
+  var lis = document.querySelector('div#grand-node');
+  while(lis.children.length === 0) {
+    lis = lis.children[0];
+  }
+  return lis;
+}
